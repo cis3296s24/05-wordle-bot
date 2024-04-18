320f6438be73e15a9319364129b946425d405f5c
Lilly Hewitt <tuo47895@temple.edu>
2024-04-18 19:57:10 -0400

    work on implementing features for the shop, updating database when features are bought

--- commands/utility/buyReveal.js
+++ commands/utility/buyReveal.js
@@ -1,10 +1,24 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+function queryReveals(id){
+    return new Promise((resolve,reject) => {
+        let sql
+        sql = ' SELECT reveals FROM users WHERE id = ?';
+        db.all(sql, [id], (err,rows)   => {
+            if (err) {
+                console.error(err.message);
+                reject(err);
+            }
+                resolve(rows[0].points);
+        });
+    });
+}
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('buy_reveal')
         .setDescription('Purchase a feature to reveal the first letter in a game.'),
     async execute(interaction) {
-        await interaction.reply('Congrats, you now have the first letter of the word revealed for game of your choice.');
+        await interaction.reply('Congrats, you bought a reveal of the first letter of the word revealed for game of your choice.');
     },
 };
